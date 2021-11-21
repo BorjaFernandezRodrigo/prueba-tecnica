6c6a37989314cfa092d49c269c99255297b100ee
BorjaFernandezRodrigo <31290988+BorjaFernandezRodrigo@users.noreply.github.com>
2021-11-21 14:41:10 +0100

    delete: componenet portfolio

--- src/app/theme/components/components.module.ts
+++ src/app/theme/components/components.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { MaterialsModule } from '../shared/materials.module';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 @NgModule({
-  declarations: [SearchComponent, PortfolioComponent, BreadcrumbComponent],
-  exports: [SearchComponent, PortfolioComponent, BreadcrumbComponent],
+  declarations: [SearchComponent, BreadcrumbComponent],
+  exports: [SearchComponent, BreadcrumbComponent],
   imports: [CommonModule, MaterialsModule],
 })
 export class ComponentsModule {}
